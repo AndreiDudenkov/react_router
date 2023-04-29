b8c4871b879352a4d333edb233e4ddd5b5fc37e7
AndreiDudenkov <andrei634d@gmail.com>
2023-04-29 22:32:29 +0300

    Router + navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {NavLink, Routes, Route, Outlet, useParams} from 'react-router-dom'
+
+const Profile = () => {
+    const some = useParams()
+    return <div>profileuseParams</div>
+}
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavLink to={'/'}>-main-</NavLink>
+            <NavLink to={'/user_login'}>-login-</NavLink>
+            <NavLink to={'/profile'} className={({isActive})=>isActive? 'used':'unUsed'}> -profile-</NavLink>
+            <NavLink to={'/profile/1'}>-profile/1-</NavLink>
+
+            <Routes>
+                <Route path={'/*'} element={<div>-Error 404-</div>}/>
+                <Route path={'/'} element={<div>-main-</div>}/>
+                <Route path={'/user_login'} element={<div>-login-</div>}/>
+                <Route path={'/profile'} element={<div>-profile-</div>}/>
+                <Route path={'/profile/:id'} element={<Profile/>}/>
+            </Routes>
+</div>
+)
 }
 
 export default App;
